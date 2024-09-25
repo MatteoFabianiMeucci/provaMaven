31e74536eb67b567accb7956a31cd41e1aba3290
lai2-24 <lai2-24@itismeucci.com>
2024-09-25 12:08:37 +0200

    Aggiunta MioThread

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,10 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int l = 200;
+        MioThread t1 = new MioThread(l);
+        MioThread t2 = new MioThread(l);
+        new Thread(t1).start();
+        new Thread(t2).start();
     }
 }
